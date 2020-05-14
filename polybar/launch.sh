8239522a7a2cffd9e72cd41f45da0293e1cf8fe3
m0e <58530674+m0e702@users.noreply.github.com>
2020-05-14 13:23:33 +0100

    Add files via upload

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -12,4 +12,4 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 # Launch bar1 and bar2
 polybar -c ~/.config/polybar/config3 top & 
 polybar -c ~/.config/polybar/config main & 
-polybar -c ~/.config/polybar/config2 top &
+
